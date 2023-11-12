40217bedf7f8eea6e884e4f53d48982fd3295ca4
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-11-12 03:13:30 -0600

    new routes

--- server/Word_meanings.py
+++ server/Word_meanings.py
@@ -1,12 +1,12 @@
 from PyDictionary import PyDictionary
 from gtts import gTTS
 
-def meaning_pronounciation(word, Language = 'en'):
+def word_meaning(word, Language = 'en'):
     dictionary1 = PyDictionary()
 
     # Finding Synonym using Dictionary1
     word_meaning = dictionary1.meaning(word)['Noun'][0] 
 
-    myobj = gTTS(text = word, lang = Language, slow=False)
-    myobj.save("pronounciation.mp3")
+    # myobj = gTTS(text = word, lang = Language, slow=False)
+    # myobj.save("pronounciation.mp3")
     return word_meaning
--- server/server.py
+++ server/server.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-#from dotenv import load_dotenv
-
-
+from dotenv import load_dotenv
+from Text_summarization import Summarization
+from Word_meanings import word_meaning
+from translator import language_translation
 
 app = Flask(__name__)
 CORS(app)
@@ -15,14 +16,19 @@ def members():
 def definition():
     data = request.get_json()
     user_word = data.get('word')
-    return jsonify({"response": "This is your word " + user_word}) #return the summarize function here
+    return jsonify({"response": "This is your word " + word_meaning(user_word)}) #return the summarize function here
 
 @app.route("/summarize", methods=['POST'])
 def summarize():
     data = request.get_json()
     para = data.get('text')
-    return jsonify({"response": para}) #return the summarize function here.
+    return jsonify({"response": Summarization(para)}) #return the summarize function here.
 
+@app.route("/translate", methods=['POST'])
+def translate():
+    data = request.get_json()
+    para = data.get('text')
+    return jsonify({"response": Summarization(para)}) #return the summarize function here.
 
 if __name__ == "__main__":
     app.run(debug = True)
